08ff140e5cb875983c1438046c3a502093972c5f
Michael Hermes <mikehermes87@gmail.com>
2021-10-15 14:53:59 -0700

    Implement API route for adding an Exercise.

--- apiRoutes/index.js
+++ apiRoutes/index.js
@@ -13,7 +13,19 @@ router.get('/api/workouts', async (req, res) => {
 
 router.get('/api/workouts/range', async (req, res) => {});
 
-router.put('/api/workouts/:id', async (req, res) => {});
+router.put('/api/workouts/:id', async (req, res) => {
+	try {
+		const exercise = await db.Exercise.create(req.body);
+		const workout = await db.Workout.findOneAndUpdate(
+			{ _id: mongoose.ObjectId(req.params.id) },
+			{ $push: { exercises: exercise._id } },
+			{ new: true }
+		);
+		res.json(workout);
+	} catch (error) {
+		res.json(error);
+	}
+});
 
 router.post('/api/workouts', async (req, res) => {
 	try {

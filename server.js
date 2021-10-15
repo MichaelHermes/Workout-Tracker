283587e0e69450748701f49ee8f0b3f0d28b1e5b
Michael Hermes <mikehermes87@gmail.com>
2021-10-15 13:39:15 -0700

    Add server route for exercise page.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
 const mongoose = require('mongoose');
 const apiRoutes = require('./apiRoutes');
 
@@ -21,6 +22,16 @@ app.use(apiRoutes);
 mongoose.connect(
 	process.env.MONGODB_URI || 'mongodb://localhost/workouttracker',
 	{ useNewUrlParser: true }
+);
+
+// GET route for the homepage
+app.get('/', (req, res) =>
+	res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET route for the exercise page
+app.get('/exercise', (req, res) =>
+	res.sendFile(path.join(__dirname, 'public/exercise.html'))
 );
 
 app.listen(PORT, () => {
